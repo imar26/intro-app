bed6ec568d5e25b4ff716728c39a6b995ab7d6d3
Aadesh Randeria <aadeshranderia26@gmail.com>
2017-10-24 17:34:14 -0400

    intro app initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/user/login/login.component';
+import { RegisterComponent } from './views/user/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
