a04903be8a280f0355196c34e2d78467b940ff8b
Aadesh Randeria <aadeshranderia26@gmail.com>
2017-10-25 22:18:40 -0400

    website list

---
+++ src/app/models/website.model.client.ts
@@ -0,0 +1,9 @@
+export class Website {
+	_id: String;
+	name: String;
+
+	constructor(_id, name) {
+		this._id = _id;
+		this.name = name;
+	}
+}
---
+++ src/app/services/website.mock.ts
@@ -0,0 +1,8 @@
+import { Website } from '../models/website.model.client';
+
+export const WEBSITES:  Website[] = [
+	new Website('123', 'Facebook'),
+	new Website('234', 'Google'),
+	new Website('345', 'Twitter'),
+	new Website('456', 'LinkedIn')
+];
